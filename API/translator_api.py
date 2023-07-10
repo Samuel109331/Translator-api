713ee347147de89d34e243f653569216934cc67e
Samuel Getachew <60266765+Samuel109331@users.noreply.github.com>
2023-07-11 02:18:12 +0530

    Add files via upload

---
+++ API/translator_api.py
@@ -0,0 +1,29 @@
+from flask_restful import Api,Resource
+from flask import Flask,make_response,render_template
+import googletrans
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+rest_api = Api(app)
+
+class LanguageList(Resource):
+    def post(self):
+        return {"langs":list(googletrans.LANGUAGES.values())}
+    def get(self):
+        return {"langs":list(googletrans.LANGUAGES.values())}
+
+class TranslateText(Resource):
+    def get(self,word,lang):
+        response = make_response({"result" : googletrans.Translator().translate(text=word,dest=lang).text})
+        return response
+    
+rest_api.add_resource(LanguageList,"/langlist")
+rest_api.add_resource(TranslateText,"/translate/<string:word>/<string:lang>")
+
+@app.route("/")
+def homePage():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
